cf58c0c7980718917c3672f43c8a5fd854a277bb
DCisHurt <roger33669@gmail.com>
2021-08-16 10:28:51 +0800

    update test file

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-
+MxMotor.init()
+basic.forever(function () {
+    serial.writeString("Battery=")
+    serial.writeLine("" + (MxMotor.readVbat()))
+    MxMotor.setPWM(MotorChannel.CH1, 255)
+    MxMotor.setPWM(MotorChannel.CH2, -255)
+})
+    
